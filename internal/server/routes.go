20e63a61fd684adf4f724c7ccc5ba8d501a50d81
Daniel McFarland <daniel.mcfarland@me.com>
2024-05-31 21:42:23 +0100

    Implementing Code to Write Bookmark

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,14 +1,17 @@
 package server
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 type tokenResponse struct {
@@ -20,13 +23,40 @@ type tokenResponse struct {
 	Nonce    int    `json:"nonce"`
 }
 
+type GitHubCommitter struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type GitHubCommit struct {
+	Message   string          `json:"message"`
+	Committer GitHubCommitter `json:"committer"`
+	Content   string          `json:"content"`
+}
+
+type DataEntryProperties struct {
+	Published  []string `json:"published"`
+	BookmarkOf []string `json:"bookmark-of"`
+	PostStatus []string `json:"post-status"`
+}
+
+type DataEntry struct {
+	Date       string              `json:"date"`
+	Deleted    bool                `json:"deleted"`
+	Draft      bool                `json:"draft"`
+	H          string              `json:"h"`
+	Properties DataEntryProperties `json:"properties"`
+	Type       string              `json:"type"`
+	Slug       string              `json:"slug"`
+	ClientId   string              `json:"client_id"`
+}
+
 func (s *Server) RegisterRoutes() http.Handler {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /micropub", s.MicropubHandler)
 	mux.HandleFunc("GET /auth", s.AuthHandler)
 	mux.HandleFunc("GET /auth/callback", s.AuthCallbackHandler)
-	mux.HandleFunc("GET /auth/token", s.AuthTokenHandler)
 
 	return mux
 }
@@ -53,21 +83,33 @@ func (s *Server) MicropubHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// ACTUALLY CREATE POST HERE
+	if !data.Has("h") {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
 
-	w.Header().Add("Location", fmt.Sprintf("https://danielmcfarland.dev/%v", ""))
-	w.WriteHeader(http.StatusCreated)
+	switch data.Get("h") {
+	case "bookmark":
+		fmt.Println(data)
+		if !data.Has("bookmark-of") {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+
+		w.Header().Add("Location", createBookmark(data.Get("bookmark-of")))
+		w.WriteHeader(http.StatusCreated)
+	}
 
 	_, _ = w.Write(nil)
 }
 
 func isTokenValid(token string) bool {
-	url := "https://tokens.indieauth.com/token"
+	tokenUrl := "https://tokens.indieauth.com/token"
 	method := "GET"
 
 	client := &http.Client{}
 
-	req, err := http.NewRequest(method, url, nil)
+	req, err := http.NewRequest(method, tokenUrl, nil)
 
 	if err != nil {
 		fmt.Println(err)
@@ -91,16 +133,16 @@ func isTokenValid(token string) bool {
 		return false
 	}
 
-	return data.Me == "http://danielmcfarland.dev/"
+	return data.Me == "https://danielmcfarland.dev/"
 }
 
 func (s *Server) AuthHandler(w http.ResponseWriter, r *http.Request) {
 	app_url := os.Getenv("APP_URL")
 
 	me := "https://danielmcfarland.dev"
+	state := randSeq(5)
 	redirect_uri := fmt.Sprintf("%v/auth/callback", app_url)
 	client_id := app_url
-	state := "abc123"
 	scope := "create"
 	response_type := "code"
 
@@ -173,12 +215,86 @@ func (s *Server) AuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(jsonResp)
 }
 
-func (s *Server) AuthTokenHandler(w http.ResponseWriter, r *http.Request) {
-	params, _ := url.ParseQuery(r.URL.RawQuery)
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-	for k, v := range params {
-		fmt.Println(fmt.Sprintf("key[%s] value[%s]\n", k, v))
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func createBookmark(bookmarkUrl string) string {
+	fmt.Println(fmt.Sprintf("create bookmark: %v", bookmarkUrl))
+
+	slug := randSeq(5)
+	fmt.Println(slug)
+	date := time.Now()
+	month := date.Month()
+	year := date.Year()
+
+	githubUrl := fmt.Sprintf("https://api.github.com/repos/danielmcfarland/danielmcfarland.dev/contents/content/data/%04d/%02d/%v.md", year, month, slug)
+	method := "PUT"
+
+	entryContent := DataEntry{
+		Date:    date.Format(time.RFC3339),
+		Deleted: false,
+		Draft:   false,
+		H:       "h-entry",
+		Properties: DataEntryProperties{
+			Published:  []string{date.Format(time.RFC3339)},
+			BookmarkOf: []string{bookmarkUrl},
+			PostStatus: []string{"published"},
+		},
+		Type:     "bookmarks",
+		Slug:     fmt.Sprintf("2024/05/%v", slug),
+		ClientId: "https://app.danielmcfarland.dev",
 	}
 
-	//fmt.Println(params)
+	fmt.Println(entryContent)
+
+	entryContentString, err := json.Marshal(entryContent)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	commitPayload := GitHubCommit{
+		Message: "Adding Bookmark",
+		Committer: GitHubCommitter{
+			Name:  "Daniel McFarland",
+			Email: "micropub@danielmcfarland.dev",
+		},
+		Content: base64.StdEncoding.EncodeToString(entryContentString),
+	}
+
+	commitPayloadString, err := json.Marshal(commitPayload)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	payload := strings.NewReader(string(commitPayloadString))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, githubUrl, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", os.Getenv("GITHUB_API_KEY")))
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	return fmt.Sprintf("https://danielmcfarland.dev/data/%04d/%02d/%v", year, month, slug)
 }
